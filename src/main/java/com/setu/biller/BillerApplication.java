05ebd35db03f2525b84e14acb19ee2336332fbaf
Rahul Raj <rahulraj@Users-MacBook-Pro-2.local>
2020-05-06 10:45:19 +0530

    Initial commit.

---
+++ src/main/java/com/setu/biller/BillerApplication.java
@@ -0,0 +1,13 @@
+package com.setu.biller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillerApplication.class, args);
+	}
+
+}
